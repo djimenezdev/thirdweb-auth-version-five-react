5a9b2f0aea295a65b3545dafeb7f529014657d4d
Daniel Jimenez <daniel@djimenezdev.com>
2024-06-23 11:04:46 -0700

    project config

---
+++ src/environment.js
@@ -0,0 +1,5 @@
+export default {
+  VITE_THIRDWEB_CLIENT_KEY: import.meta.env.VITE_THIRDWEB_CLIENT_KEY || "",
+  VITE_WALLET_CONNECT_ID: import.meta.env.VITE_WALLET_CONNECT_ID || "",
+  VITE_API_URL: import.meta.env.VITE_API_URL || "",
+};

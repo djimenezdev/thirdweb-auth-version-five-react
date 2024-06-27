af00d5af08c81afbc30a61724dfed1897ac5481b
Daniel Jimenez <daniel@djimenezdev.com>
2024-06-27 15:54:19 -0700

    added transaction test and unit converter

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,59 @@
-import { createThirdwebClient, defineChain } from "thirdweb";
 import Web3Provider from "./Web3Provider";
 import { ConnectButton } from "thirdweb/react";
 import { createWallet, walletConnect, inAppWallet } from "thirdweb/wallets";
 import SignData from "./SignData";
 import environment from "./environment";
-// import { get, post } from "./helpers";
+import GetUnits from "./GetUnits";
+import { polygon, sepolia } from "thirdweb/chains";
+import { client } from "./thirdweb";
+import TransactionTest from "./TransactionTest";
+import { post } from "./helpers";
 
-const client = createThirdwebClient({
-  clientId: environment.VITE_THIRDWEB_CLIENT_KEY,
+const metaMask = createWallet("io.metamask");
+const coinbase = createWallet("com.coinbase.wallet");
+const trustWallet = createWallet("com.trustwallet.app");
+const walletConnectInit = walletConnect();
+const inAppWalletInit = inAppWallet({
+  auth: {
+    options: ["email", "google", "phone"],
+  },
+});
+
+metaMask.subscribe("disconnect", async () => {
+  await post({
+    url: environment.VITE_API_URL + "/logout",
+  });
+  console.log("Metamask disconnected");
+});
+
+coinbase.subscribe("disconnect", async () => {
+  await post({
+    url: environment.VITE_API_URL + "/logout",
+  });
+  console.log("Coinbase disconnected");
+});
+
+trustWallet.subscribe("disconnect", async () => {
+  await post({
+    url: environment.VITE_API_URL + "/logout",
+  });
+  console.log("Trust Wallet disconnected");
+});
+
+walletConnectInit.subscribe("disconnect", async () => {
+  await post({
+    url: environment.VITE_API_URL + "/logout",
+  });
+  console.log("Wallet Connect disconnected");
 });
 
 const wallets = [
-  createWallet("io.metamask"),
-  createWallet("com.coinbase.wallet"),
-  createWallet("com.trustwallet.app"),
-  walletConnect({
-    projectId: environment.VITE_WALLET_CONNECT_ID,
-  }),
-  inAppWallet({
-    auth: {
-      options: ["email", "google", "phone"],
-    },
-  }),
+  metaMask,
+  coinbase,
+  trustWallet,
+  walletConnectInit,
+  inAppWalletInit,
 ];
-
-const polygon = defineChain({
-  id: 137,
-});
 
 function App() {
   return (
@@ -38,8 +65,13 @@ function App() {
           theme={"dark"}
           connectModal={{ size: "wide" }}
           showAllWallets={false}
-          recommendedWallets={[wallets[0], wallets[1]]}
-          chain={polygon}
+          recommendedWallets={[wallets[0]]}
+          chain={environment.VITE_ACTIVE_CHAIN === "137" ? polygon : sepolia}
+          onDisconnect={async () => {
+            await post({
+              url: environment.VITE_API_URL + "/logout",
+            });
+          }}
           /* auth={{
             getLoginPayload: async (params) => {
               return get({
@@ -69,6 +101,8 @@ function App() {
           }} */
         />
         <SignData polygon={polygon} />
+        <GetUnits />
+        <TransactionTest />
       </div>
     </Web3Provider>
   );
